b5d32fdb1fb53eb64c1c8eaf03d29e56c340d23e
Inakir <sysadmin@sec.tamu.edu>
2017-04-17 18:10:13 -0500

    neural network
    works!!!!

--- neuralgame/main.cpp
+++ neuralgame/main.cpp
@@ -15,92 +15,6 @@
 
 using namespace std;
 
-// Silly class to read training data from a text file -- Replace This.
-// Replace class TrainingData with whatever you need to get input data into the
-// program, e.g., connect to a database, or take a stream of data from stdin, or
-// from a file specified by a command line argument, etc.
-
-class TrainingData
-{
-public:
-	TrainingData(const string filename);
-	bool isEof(void) { return m_trainingDataFile.eof(); }
-	void getTopology(vector<unsigned> &topology);
-
-	// Returns the number of input values read from the file:
-	unsigned getNextInputs(vector<double> &inputVals);
-	unsigned getTargetOutputs(vector<double> &targetOutputVals);
-
-private:
-	ifstream m_trainingDataFile;
-};
-
-void TrainingData::getTopology(vector<unsigned> &topology)
-{
-	string line;
-	string label;
-
-	getline(m_trainingDataFile, line);
-	stringstream ss(line);
-	ss >> label;
-	if (this->isEof() || label.compare("topology:") != 0) {
-		abort();
-	}
-
-	while (!ss.eof()) {
-		unsigned n;
-		ss >> n;
-		topology.push_back(n);
-	}
-
-	return;
-}
-
-TrainingData::TrainingData(const string filename)
-{
-	m_trainingDataFile.open(filename.c_str());
-}
-
-unsigned TrainingData::getNextInputs(vector<double> &inputVals)
-{
-	inputVals.clear();
-
-	string line;
-	getline(m_trainingDataFile, line);
-	stringstream ss(line);
-
-	string label;
-	ss >> label;
-	if (label.compare("in:") == 0) {
-		double oneValue;
-		while (ss >> oneValue) {
-			inputVals.push_back(oneValue);
-		}
-	}
-
-	return inputVals.size();
-}
-
-unsigned TrainingData::getTargetOutputs(vector<double> &targetOutputVals)
-{
-	targetOutputVals.clear();
-
-	string line;
-	getline(m_trainingDataFile, line);
-	stringstream ss(line);
-
-	string label;
-	ss >> label;
-	if (label.compare("out:") == 0) {
-		double oneValue;
-		while (ss >> oneValue) {
-			targetOutputVals.push_back(oneValue);
-		}
-	}
-
-	return targetOutputVals.size();
-}
-
 void showVectorVals(string label, vector<double> &v)
 {
 	cout << label << " ";
@@ -111,48 +25,91 @@ void showVectorVals(string label, vector<double> &v)
 	cout << endl;
 }
 
-
 int main()
 {
-	TrainingData trainData("input.txt");
+	cout << "Enter 64 inputs of either 1 or 0. " << endl;
 
-	// e.g., { 3, 2, 1 }
 	vector<unsigned> topology;
-	trainData.getTopology(topology);
 
-	Net myNet(topology);
+	topology = { 2, 4, 1 };
+	Net nnet(topology);
 
-	vector<double> inputVals, targetVals, resultVals;
+	vector<double> inputs, targets, results;
 	int trainingPass = 0;
 
-	while (!trainData.isEof()) {
+	vector<double> input_vals, target_vals;
+	input_vals =
+	{ 1.0, 0.0,
+		1.0, 1.0,
+		1.0, 0.0,
+		0.0, 1.0,
+		0.0, 1.0,
+		0.0, 1.0,
+		0.0, 1.0,
+		1.0, 1.0,
+		1.0, 1.0,
+		0.0, 1.0,
+		0.0, 0.0,
+		0.0, 1.0,
+		0.0, 0.0,
+		0.0, 0.0,
+		1.0, 0.0 };
+	target_vals =
+	{ 1.0,
+		0.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		1.0,
+		0.0,
+		0.0,
+		1.0,
+		0.0,
+		1.0,
+		0.0,
+		0.0,
+		1.0 };
+	int i = 0;
+	while (trainingPass<2000)
+	{
+		if (i >= target_vals.size())
+		{
+			i = 0;
+		}
 		++trainingPass;
 		cout << endl << "Pass " << trainingPass;
 
-		// Get new input data and feed it forward:
-		if (trainData.getNextInputs(inputVals) != topology[0]) {
-			break;
-		}
-		showVectorVals(": Inputs:", inputVals);
-		myNet.feedForward(inputVals);
+		inputs.clear();
+		targets.clear();
 
-		// Collect the net's actual output results:
-		myNet.getResults(resultVals);
-		showVectorVals("Outputs:", resultVals);
+		inputs.push_back(input_vals[i * 2]);
+		inputs.push_back(input_vals[i * 2 + 1]);
+		targets.push_back(target_vals[i]);
 
-		// Train the net what the outputs should have been:
-		trainData.getTargetOutputs(targetVals);
-		showVectorVals("Targets:", targetVals);
-		assert(targetVals.size() == topology.back());
+		showVectorVals(": Inputs: ", inputs);
 
-		myNet.backProp(targetVals);
+		nnet.feedForward(inputs);
 
-		// Report how well the training is working, average over recent samples:
-		cout << "Net recent average error: "
-			<< myNet.getRecentAverageError() << endl;
+		//collect the net's actual results
+		nnet.getResults(results);
+		showVectorVals(": Outputs: ", results);
+
+		//train the net on what outputs should have been
+		showVectorVals(": Targets: ", targets);
+		//assert(targets.size() == topology.back());
+
+		nnet.backProp(targets);
+
+		//report how well the training is working
+		cout << "Net recent average error: " << nnet.getRecentAverageError() << endl;
+		++i;
 	}
 
 	cout << endl << "Done" << endl;
-	string temp;
-	cin>>temp;
+
+	//inputs = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+
+	int temp;
+	cin >> temp;
 }
