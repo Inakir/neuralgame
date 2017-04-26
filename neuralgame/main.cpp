94e9c6ef5a99516bd06626cb13919fa60c0a9a6f
Inakir <spainiards@hotmail.com>
2017-04-25 22:15:35 -0500

    the game

--- neuralgame/main.cpp
+++ neuralgame/main.cpp
@@ -32,6 +32,7 @@ double avg_blue = 0;
 double avg_red = 0;
 double bluesz = .000001;
 double redsz = .000001;
+int score = 0; 
 
 void endGame()
 {
@@ -39,8 +40,8 @@ void endGame()
 	user_training = "\nTraining Choices: ";
 	user_choices = "\nYour Choices: ";
 	AI_choices = "\nAI Guesses:   ";
-
-	for (int i = 0; i < 32; i++) {
+	//0-31 32-63 64-95
+	for (int i = 0; i <= 63; i++) {
 
 		if (user_inputs[i] == 1.0)
 		{
@@ -52,7 +53,7 @@ void endGame()
 		}
 	}
 
-	for (int i = 1; i < 65; i++) {
+	for (int i = 64; i <= 95; i++) {
 
 		if (user_inputs[i] == 1.0)
 		{
@@ -66,7 +67,7 @@ void endGame()
 	
 	for (int i = 0; i < 32; i++)
 	{
-		if (AI_guesses[i] == user_inputs[i + 65])
+		if (AI_guesses[i] == user_inputs[i + 64])
 		{
 			correct_guesses++;
 		}
@@ -115,9 +116,10 @@ void showVectorVals(string label, vector<double> &v)
 //t = 1: User inputs < 64
 //t = 2: User inputs > 64, AI now guessing 
 //t > 2: Game is done
-void setText(int i)
+void setText()
 {
-	if (i == 0)
+	//0-31 32-63 64-95
+	if (user_inputs.size() == 0)
 	{
 		instr = "Instructions: ";
 		instr += "\nThis is a guessing game. You are gonna press the buttons below 65 times trying to be unpredicatable. ";
@@ -125,26 +127,41 @@ void setText(int i)
 		instr += "\n however this time the game will try and predict your choice. ";
 		instr += "\nTheoretically we should only be able to predict 50% of your choices, we'll see how that goes ;). \n\n\n\n";
 	}
-	else if (i == 1)
+	else if (user_inputs.size() < 64)
 	{
 		instr = "Choices left to go before the AI starts guessing: ";
-		instr += to_string((65 - user_inputs.size()));
+		instr += to_string((64 - user_inputs.size()));
 		instr += "\n";
 		instr += user_training;
 	}
-	else if (i == 2)
+	else if (user_inputs.size() <= 95)
 	{
 		instr = "The AI is now guessing what your inputs are \n\n";
 		instr += "Choices left: ";
-		instr += to_string((97 - user_inputs.size()));
+		instr += to_string((96 - user_inputs.size()));
 		instr += "\n";
 		instr += user_training;
 		instr += user_choices;
 		instr += AI_choices;
+		if (user_inputs.size() == 64)
+		{
+			instr += "\n";
+		}
+		else if (user_inputs.back() == AI_guesses[AI_guesses.size()-2])
+		{
+			instr += "\nAI correct!\n";
+			score++;
+		}
+		else //size == 96
+		{
+			instr += "\nAI wrong!\n";
+		}
+		instr += to_string(score);
+		instr += "/32";
 	}
 	else
 	{
-		instr = "Game is over";
+		instr = "Game is over\n";
 		instr += "Choices left: 0\n";
 		instr += user_training;
 		instr += user_choices;
@@ -224,117 +241,126 @@ void train()
 
 void AI()
 {
-	if (user_inputs.size() >= 97)	//After 65 inputs the game ends (33 inputs for training phase, then 32 more for actual testing
+	//0-31 32-63 64-95
+	if (user_inputs.size() == 64)
 	{
-		endGame();
-		setText(3);
+		train(); train(); train(); train(); train(); train(); train();
 	}
-	else if (user_inputs.size() > 64)	//Goes on here after 64 inputs and starts guessing
+		
+	//trains the net
+	train();
+		
+	//uses previous statistics to determine how to guess red or blue
+	avg_red /= redsz;
+	avg_blue /= bluesz;
+	double mid = (avg_red + avg_blue) / 2;
+	bool correct = avg_red>avg_blue;
+		
+	//after training we're gonna go through and guess
+	inputs.clear();
+	targets.clear();
+		
+	//this fills in the user inputs
+	fillValues(user_inputs.size() - 33);		
+		
+	//feeds the inputs to the net
+	nnet.feedForward(inputs);
+		
+	//collect the net's actual results
+	nnet.getResults(results);
+		
+	//This next line is for debugging purposes
+	//cout << correct << " " << avg_red << " " << avg_blue << endl;
+		
+	if (results[0] < mid) 
 	{
-		if (user_inputs.size() == 65)
+		if (correct) 
 		{
-			train(); train(); train(); train(); train(); train(); train();
+			AI_guesses.push_back(0.0);
+			//AI_choices += "b";
+			AI_choices += "x";
 		}
-		setText(2);
-		
-		//trains the net
-		train();
-		
-		//uses previous statistics to determine how to guess red or blue
-		avg_red /= redsz;
-		avg_blue /= bluesz;
-		double mid = (avg_red + avg_blue) / 2;
-		bool correct = avg_red>avg_blue;
-		
-		//after training we're gonna go through and guess
-		inputs.clear();
-		targets.clear();
-		
-		//this fills in the user inputs
-		fillValues(user_inputs.size() - 33);		
-		
-		//feeds the inputs to the net
-		nnet.feedForward(inputs);
-		
-		//collect the net's actual results
-		nnet.getResults(results);
-		
-		//This next line is for debugging purposes
-		//cout << correct << " " << avg_red << " " << avg_blue << endl;
-		
-		if (results[0] < mid) 
+		else 
 		{
-			if (correct) 
-			{
-				AI_guesses.push_back(0.0);
-				//AI_choices += "b";
-				AI_choices += "x";
-			}
-			else 
-			{
-				AI_guesses.push_back(1.0);
-				//AI_choices += "r";
-				AI_choices += "x";
-			}
+			AI_guesses.push_back(1.0);
+			//AI_choices += "r";
+			AI_choices += "x";
+		}
+	}
+	else {
+		if (correct) {
+			AI_guesses.push_back(1.0);
+			//AI_choices += "r";
+			AI_choices += "x";
 		}
 		else {
-			if (correct) {
-				AI_guesses.push_back(1.0);
-				//AI_choices += "r";
-				AI_choices += "x";
-			}
-			else {
-				AI_guesses.push_back(0.0);
-				//AI_choices += "b";
-				AI_choices += "x";
-			}
+			AI_guesses.push_back(0.0);
+			//AI_choices += "b";
+			AI_choices += "x";
 		}
-	}
-	else if (user_inputs.size() > 32)	//Goes on here after 33 inputs have been recorded
-	{
-		setText(1);
-		//train();
-	}
-	else
-	{
-		setText(1);
 	}
 }
 
+
+//0-31 32-63 64-95
 static void redPress(Fl_Widget *w, void* data) 
 {
-	if (user_inputs.size() <= 64) 
+	if (user_inputs.size() < 64) 
 	{ 
 		user_inputs.push_back(1.0);
 		//user_training += "r";
 		user_training += "x";
-		AI();
+		if (user_inputs.size() == 63)
+		{
+			AI();
+		}
 	}
-	else						  
+	else 
 	{ 
 		//user_choices += "r";
 		user_choices  += "x";
-		AI();
 		user_inputs.push_back(1.0);
+		if (user_inputs.size() < 96)
+		{
+			AI();
+		}
+		else //After 96 inputs the game ends (33 inputs for training phase, then 32 more for actual testing
+		{
+			endGame();
+		}
 	}
+	setText();
 }
 
 static void bluePress(Fl_Widget *w, void* data) 
 {
-	if (user_inputs.size() <= 64) 
+	//fills in the user inputs. After 64 values have been inputted the AI starts guessing. The AI retrains itself before guessing. This is why the call to the fuction AI got moved up in 
+	//the else statement, because it'd be cheating if it trained itself with the results already in the list so it doesn't add that value to the list until after the guess.
+	if (user_inputs.size() < 64) 
 	{ 
 		user_inputs.push_back(0.0);
 		//user_training += "b";
 		user_training += "x";
-		AI();
+		if (user_inputs.size() == 63)
+		{
+			AI();
+		}
 	}
-	else						  
+	else
 	{
 		//user_choices += "b";
 		user_choices  += "x";
-		AI();
 		user_inputs.push_back(0.0);
+		if(user_inputs.size() < 96)
+		{
+			AI();
+		}
+		else //After 96 inputs the game ends (33 inputs for training phase, then 32 more for actual testing
+		{
+			endGame();
+		}
 	}
+	setText();
 }
 
 int main()
@@ -342,7 +368,7 @@ int main()
 	//FLTK Window init
 	
 	//Box Init
-	setText(0);
+	setText();
 	Fl_Box box(50, 100, 700, 20, instr.c_str());
 	
 	//Buttons
